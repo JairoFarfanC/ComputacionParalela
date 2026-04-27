3bc509f6ab3a86330a60683d15cb93523f3129bf
XeihtT <miglesiaq@alumnos.unex.es>
2026-04-27 16:58:28 +0200

    Version secuencial acabada

--- mmatrices-secuencial/main.cpp
+++ mmatrices-secuencial/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 
     if (argc < 2){
         std::cout<< "Tienes que ejecutar pasando el tamaño por parámetro, por ejemplo:\n" << std::endl;
-        std::cout<< "./mmatrices_cuda 1024\n" << std::endl;
+        std::cout<< "./mmatrices_secuencial 1024\n" << std::endl;
 
         return -1;
     }
